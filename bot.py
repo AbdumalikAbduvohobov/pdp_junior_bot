26841a52cb0ed9059720bd93437c5a412a9a5c00
Abdumalik <abdumalikabduvohobov341@gmail.com>
2025-03-27 10:28:33 +0300

    \session qo'shildi

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
 
+from bot_menu import set_bot_menu
 from handlers.commands import router as commands_router
 from handlers.handlers import router as handlers_router
-from bot_menu import set_bot_menu
+from aiogram.client.session.aiohttp import AiohttpSession
+
 load_dotenv()
+session = AiohttpSession(proxy="http://proxy.server:3128")
 
 TOKEN = getenv("BOT_TOKEN")
 
@@ -18,7 +21,8 @@ dp.include_router(commands_router)
 dp.include_router(handlers_router)
 
 async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(token=TOKEN, session=session)
     await set_bot_menu(bot)
     await dp.start_polling(bot)
 
